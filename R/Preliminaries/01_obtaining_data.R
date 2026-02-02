41289cd554759eca13b39d588f6a229df36ba6d6
Matt <mcmccarthywork@gmail.com>
2026-02-02 14:45:42 +0000

    Clean up formatting for report.

--- R/Preliminaries/01_obtaining_data.R
+++ R/Preliminaries/01_obtaining_data.R
@@ -1,13 +1,12 @@
 rm(list = ls())
 
-install.packages("OpenML") # Contains old dataset
-install.packages("farff") # Need for reading in from OpenML
+install.packages(c("OpenML", "farff"))
 
-library(OpenML)
-library(farff)
+library(OpenML) # Contains desired dataset
+library(farff) # Needed to load in from OpenML
 
 ds <- getOMLDataSet(data.id = 41214)
-freMTPL2freq_raw <- ds$data # Right dimensions, is the dataset we need
+freMTPL2freq_raw <- ds$data # Correct dimensions, is the dataset we need
 
 # Pushing csv to GitHub for easy read-in.
 write.csv(freMTPL2freq_raw, "freMTPL2freq_raw.csv", row.names = FALSE)
